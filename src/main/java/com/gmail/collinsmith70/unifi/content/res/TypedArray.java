d51a7f2678140825d2c42d87184c0bca27811300
Collin Smith <collinsmith70@gmail.com>
2016-05-14 15:47:36 -0700

    Updated TypedArray

--- src/main/java/com/gmail/collinsmith70/unifi/content/res/TypedArray.java
+++ src/main/java/com/gmail/collinsmith70/unifi/content/res/TypedArray.java
@@ -1,6 +1,5 @@
 package com.gmail.collinsmith70.unifi.content.res;
 
-import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.Size;
