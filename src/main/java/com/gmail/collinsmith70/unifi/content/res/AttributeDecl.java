934307825e97a47b3e8e2dea9390b217182fa207
Collin Smith <collinsmith70@gmail.com>
2016-05-17 14:38:27 -0700

    Added support for namespaces within AttributeDecl

--- src/main/java/com/gmail/collinsmith70/unifi/content/res/AttributeDecl.java
+++ src/main/java/com/gmail/collinsmith70/unifi/content/res/AttributeDecl.java
@@ -7,9 +7,13 @@ import org.apache.commons.lang3.Validate;
 import com.gmail.collinsmith70.unifi.annotation.Index;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public class AttributeDecl<T> {
 
+    @Nullable
+    private final String namespace;
+    
     @NonNull
     private final String name;
     
@@ -20,12 +24,14 @@ public class AttributeDecl<T> {
     private final int index;
     
     public AttributeDecl(@Index int index,
+                         @Nullable String namespace,
                          @NonNull String name,
                          @NonNull Class<T> type) {
         Validate.isTrue(index >= 0, "index cannot be less than 0");
         Validate.isTrue(name != null, "name cannot be null");
         Validate.isTrue(type != null, "type cannot be null");
         this.index = index;
+        this.namespace = namespace;
         this.name = name;
         this.type = type;
     }
@@ -33,6 +39,11 @@ public class AttributeDecl<T> {
     @Index
     public int getIndex() {
         return index;
+    }
+    
+    @Nullable
+    public String getNamespace() {
+        return namespace;
     }
 
     @NonNull
@@ -47,8 +58,9 @@ public class AttributeDecl<T> {
 
     @Override
     public String toString() {
-        return String.format(Locale.ROOT, "AttributeDecl: { id=%d name=\"%s\", type=\"%s\" }",
-                index, name, type.getName());
+        return String.format(Locale.ROOT,
+                "AttributeDecl: { id=%d namespace=\"%s\" name=\"%s\", type=\"%s\" }",
+                index, namespace, name, type.getName());
     }
     
 }
