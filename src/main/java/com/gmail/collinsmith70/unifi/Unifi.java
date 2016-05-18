d01831c3e2fc1001f9fe88179b3f820d0941f593
Collin Smith <collinsmith70@gmail.com>
2016-05-18 14:08:14 -0700

    Added Unifi class to store Unifi.NAMESPACE string

---
+++ src/main/java/com/gmail/collinsmith70/unifi/Unifi.java
@@ -0,0 +1,10 @@
+package com.gmail.collinsmith70.unifi;
+
+public class Unifi {
+
+  private Unifi() {
+  }
+  
+  public static final String NAMESPACE = "http://schemas.unifi.com/res/unifi";
+  
+}
