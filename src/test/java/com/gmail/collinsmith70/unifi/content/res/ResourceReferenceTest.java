e6e8f2c3e6cf58d94ffa23e805ac78dd6f496570
Collin Smith <collinsmith70@gmail.com>
2016-05-22 12:47:10 -0700

    Added UnifiTest to store Unifi test package info, such as test package name Updated ResourceReferenceTest to include #parse(String,String) refactor Added test for null default package

--- src/test/java/com/gmail/collinsmith70/unifi/content/res/ResourceReferenceTest.java
+++ src/test/java/com/gmail/collinsmith70/unifi/content/res/ResourceReferenceTest.java
@@ -6,17 +6,20 @@ import java.text.ParseException;
 
 import org.junit.Test;
 
+import com.gmail.collinsmith70.unifi.Unifi;
+import com.gmail.collinsmith70.unifi.UnifiTest;
+
 public class ResourceReferenceTest {
 
   @Test
   public void testParse_Color() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@unifi:color/white");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@unifi:color/white");
     assertTrue(ref.isStyleAttributeReference() == false);
     assertTrue(ref.getPackageName().equals("unifi"));
     assertTrue(ref.getResourceType() == ResourceReference.Type.color);
     assertTrue(ref.getResourceName().equals("white"));
     ref.recycle();
-    ref = ResourceReference.parse("@color/white");
+    ref = ResourceReference.parse(UnifiTest.PACKAGE, "@color/white");
     assertTrue(ref.isStyleAttributeReference() == false);
     assertTrue(ref.getPackageName() == null);
     assertTrue(ref.getResourceType() == ResourceReference.Type.color);
@@ -26,13 +29,13 @@ public class ResourceReferenceTest {
 
   @Test
   public void testParse_String() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@unifi:string/test");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@unifi:string/test");
     assertTrue(ref.isStyleAttributeReference() == false);
     assertTrue(ref.getPackageName().equals("unifi"));
     assertTrue(ref.getResourceType() == ResourceReference.Type.string);
     assertTrue(ref.getResourceName().equals("test"));
     ref.recycle();
-    ref = ResourceReference.parse("@string/test");
+    ref = ResourceReference.parse(UnifiTest.PACKAGE, "@string/test");
     assertTrue(ref.isStyleAttributeReference() == false);
     assertTrue(ref.getPackageName() == null);
     assertTrue(ref.getResourceType() == ResourceReference.Type.string);
@@ -41,68 +44,74 @@ public class ResourceReferenceTest {
   }
   
   @Test(expected = IllegalArgumentException.class)
-  public void testParse_null() throws ParseException {
-    ResourceReference ref = ResourceReference.parse(null);
+  public void testParse_nullPackage() throws ParseException {
+    ResourceReference ref = ResourceReference.parse(null, "");
+    ref.recycle();
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testParse_nullResourceId() throws ParseException {
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, null);
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
-  public void testParse_empty() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("");
+  public void testParse_emptyResourceId() throws ParseException {
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingReferenceTypeWithPackageTypeName() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("package:type/name");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "package:type/name");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingReferenceTypeWithTypeName() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("type/name");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "type/name");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingPackageWithTypeName() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@:type/name");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@:type/name");
     ref.recycle();
   }
 
   @Test(expected = ParseException.class)
   public void testParse_missingTypeWithoutPackage() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@/name");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@/name");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingTypeAfterPackage() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@package:/name");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@package:/name");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingNameAfterPackageType() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@package:type/");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@package:type/");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingNameAfterPackageTypeWithoutSlash() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@package:type");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@package:type");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingNameAfterType() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@type/");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@type/");
     ref.recycle();
   }
   
   @Test(expected = ParseException.class)
   public void testParse_missingNameAfterTypeWithoutSlash() throws ParseException {
-    ResourceReference ref = ResourceReference.parse("@type");
+    ResourceReference ref = ResourceReference.parse(UnifiTest.PACKAGE, "@type");
     ref.recycle();
   }
   
