1be84ea0ee5af5a7bb35c93e7e406025865427fd
Collin Smith <collinsmith70@gmail.com>
2016-07-03 01:10:08 -0700

    Documented Unifi

--- src/main/java/com/gmail/collinsmith70/unifi/Unifi.java
+++ src/main/java/com/gmail/collinsmith70/unifi/Unifi.java
@@ -1,12 +1,24 @@
 package com.gmail.collinsmith70.unifi;
 
+/**
+ * Class containing static members which are common across all modules of the Unifi platform.
+ */
 public class Unifi {
 
+  /**
+   * Enforces the uninstantiability of {@link Unifi}.
+   */
   private Unifi() {
   }
   
+  /**
+   * Default Unifi namespace.
+   */
   public static final String NAMESPACE = "http://schemas.unifi.com/res/unifi";
-  
+
+  /**
+   * Default Unifi package.
+   */
   public static final String PACKAGE = "unifi";
   
 }
