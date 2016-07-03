bb9c40f098278adb387283f825e6850b9161cf28
Collin Smith <collinsmith70@gmail.com>
2016-07-03 01:42:01 -0700

    Documented DataUtils

--- src/main/java/com/gmail/collinsmith70/unifi/util/DataUtils.java
+++ src/main/java/com/gmail/collinsmith70/unifi/util/DataUtils.java
@@ -1,11 +1,36 @@
 package com.gmail.collinsmith70.unifi.util;
 
+import com.gmail.collinsmith70.unifi.annotation.Unsigned;
+
 import android.support.annotation.Nullable;
 
+/**
+ * Class containing static utility methods which can be used when parsing data.
+ */
 public class DataUtils {
 
-  private DataUtils() {}
+  /**
+   * Enforces the uninstantiability of {@link DataUtils}.
+   */
+  private DataUtils() {
+  }
 
+  /**
+   * Attempts to parse the specified {@code String} as a boolean value, returning
+   * {@code defaultValue} in the case which the passed {@code String} is {@code null}.
+   * 
+   * A {@code String} is considered equal to boolean {@code true} if it matches:
+   * <ul>
+   *    <li>{@code 1}</li>
+   *    <li>{@code true} (case-insensitive)</li>
+   * </ul>
+   * 
+   * @param str          {@code String} to parse
+   * @param defaultValue Value to return in the case which the passed {@code String} is {@code null}
+   * 
+   * @return {@code true} if the passed {@code String} is equal to logical {@code true}, otherwise
+   *         {@code false}
+   */
   public static final boolean parseBoolean(@Nullable String str, boolean defaultValue) {
     if (str == null) {
       return defaultValue;
@@ -18,6 +43,20 @@ public class DataUtils {
     return str.toString().equalsIgnoreCase("true");
   }
 
+  /**
+   * Attempts to parse the specified {@code String} as an integer value, returning
+   * {@code defaultValue} in the cases which the passed {@code String} is {@code null} or empty.
+   * The {@code String} will be parsed in a similar manner to that of
+   * {@link Integer#decode(String)}.
+   * 
+   * @param str          {@code String} to parse
+   * @param defaultValue Value to return in the case which the passed {@code String} is {@code null}
+   *                         or empty.
+   * 
+   * @return Decoded integer value of the {@code String}.
+   * 
+   * @see Integer#decode(String)
+   */
   public static final int parseInt(@Nullable String str, int defaultValue) {
     if (str == null) {
       return defaultValue;
@@ -62,8 +101,23 @@ public class DataUtils {
     }
   }
   
+  /**
+   * Attempts to parse the specified {@code String} as an encoded {@code ARGB8888} color, returning
+   * {@code defaultValue} in the cases which the passed {@code String} is {@code null} or not a
+   * valid parseable color.
+   * 
+   * @param str          {@code String} to parse
+   * @param defaultValue Value to return in the cases which the passed {@code String} is
+   *                         {@code null} or not a valid parseable color.
+   * 
+   * @return Decoded unsigned integer representation of the passed {@code String} encoded in
+   *             {@code ARGB8888} format
+   * 
+   * {@see ColorUtils#parseColor}
+   */
   @Nullable
-  public static final int parseColor(@Nullable String str, @Nullable int defaultValue) {
+  @Unsigned
+  public static final int parseColor(@Nullable String str, @Unsigned int defaultValue) {
     if (str == null) {
       return defaultValue;
     }
