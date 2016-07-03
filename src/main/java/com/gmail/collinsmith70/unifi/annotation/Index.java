1d924bee18818a2903137a905a9512e1f39b9cd8
Collin Smith <collinsmith70@gmail.com>
2016-07-03 01:07:12 -0700

    Documented Index and Unsigned annotations

--- src/main/java/com/gmail/collinsmith70/unifi/annotation/Index.java
+++ src/main/java/com/gmail/collinsmith70/unifi/annotation/Index.java
@@ -9,6 +9,9 @@ import static java.lang.annotation.RetentionPolicy.CLASS;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Marks an integer field as an index. Indexes should be countable integers.
+ */
 @Retention(CLASS)
 @Target({METHOD, PARAMETER, FIELD, LOCAL_VARIABLE})
 public @interface Index {
