b24cffdd8c282fd00a27a0088b2b937985466f25
Collin Smith <collinsmith70@gmail.com>
2016-10-02 23:27:56 -0700

    Added setColor

--- src/main/java/com/gmail/collinsmith70/unifi/graphics/Paint.java
+++ src/main/java/com/gmail/collinsmith70/unifi/graphics/Paint.java
@@ -2,6 +2,8 @@ package com.gmail.collinsmith70.unifi.graphics;
 
 import android.support.annotation.NonNull;
 
+import org.apache.commons.lang3.Validate;
+
 public class Paint {
 
   @NonNull
@@ -13,6 +15,15 @@ public class Paint {
   @NonNull
   public Color getColor() {
     return color;
+  }
+
+  protected void _setColor(@NonNull Color color) {
+    Validate.isTrue(color != null, "color cannot be null");
+    this.color = color;
+  }
+
+  public void setColor(@NonNull Color color) {
+    _setColor(color);
   }
 
 }
