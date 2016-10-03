d0c1e55ece5c9680d07d763efa0c8fdd349ad175
Collin Smith <collinsmith70@gmail.com>
2016-10-02 23:25:34 -0700

    Added Color field

--- src/main/java/com/gmail/collinsmith70/unifi/graphics/Paint.java
+++ src/main/java/com/gmail/collinsmith70/unifi/graphics/Paint.java
@@ -1,6 +1,11 @@
 package com.gmail.collinsmith70.unifi.graphics;
 
+import android.support.annotation.NonNull;
+
 public class Paint {
+
+  @NonNull
+  private Color color;
 
   private Paint() {
   }
