53eb9d4f6eeb952598b8030523daa2e8ca3a3326
Collin Smith <collinsmith70@gmail.com>
2017-02-22 16:53:52 -0800

    Created Drawable and AbstractDrawable implementation

---
+++ src/main/java/unifi/graphics/drawable/AbstractDrawable.java
@@ -0,0 +1,314 @@
+package unifi.graphics.drawable;
+
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
+
+import unifi.graphics.Insets;
+import unifi.graphics.Rect;
+import unifi.util.LayoutDirection.ResolvedLayoutDir;
+import unifi.util.StateSet;
+
+/**
+ * Abstract implementation of a {@link Drawable}.
+ *
+ * @see Drawable
+ */
+public abstract class AbstractDrawable implements Drawable {
+  private static final Rect ZERO_BOUNDS_RECT = new Rect();
+
+  /**
+   * Set of primitive {@code int} values representing the state of this drawable
+   */
+  @NonNull
+  private int[] mStateSet = StateSet.WILD_CARD;
+
+  /**
+   * Indicates the level of this drawable.
+   *
+   * @see #setLevel(int)
+   */
+  @IntRange(from = 0, to = 10000)
+  private int mLevel = 0;
+
+  /**
+   * Bounds of this drawable, lazily initialized as a new {@link Rect} when
+   * required.
+   */
+  @NonNull
+  private Rect mBounds = ZERO_BOUNDS_RECT;
+
+  /**
+   * Weak reference to the callback associated with this drawable.
+   */
+  private WeakReference<Callback> mCallback = null;
+
+  /**
+   * Indicates whether or not this drawable is visible.
+   *
+   * @see #setVisible(boolean, boolean)
+   */
+  private boolean mVisible = true;
+
+  /**
+   * The resolved layout direction of this drawable.
+   */
+  @ResolvedLayoutDir
+  private int mLayoutDirection;
+
+  @Override
+  public void setCallback(@NonNull Callback callback) {
+    mCallback = new WeakReference<>(callback);
+  }
+
+  @Nullable
+  @Override
+  public Callback getCallback() {
+    if (mCallback != null) {
+      return mCallback.get();
+    }
+
+    return null;
+  }
+
+  @Override
+  public void invalidateSelf() {
+    final Callback callback = getCallback();
+    if (callback != null) {
+      callback.invalidateDrawable(this);
+    }
+  }
+
+  @Override
+  public void scheduleSelf(@NonNull Runnable what, long when) {
+    final Callback callback = getCallback();
+    if (callback != null) {
+      callback.scheduleDrawable(this, what, when);
+    }
+  }
+
+  @Override
+  public void unscheduleSelf(@NonNull Runnable what) {
+    final Callback callback = getCallback();
+    if (callback != null) {
+      callback.unscheduleDrawable(this, what);
+    }
+  }
+
+  @Override
+  public void setBounds(int left, int top, int right, int bottom) {
+    Rect oldBounds = mBounds;
+    if (oldBounds == ZERO_BOUNDS_RECT) {
+      oldBounds = mBounds = new Rect();
+    }
+
+    if (oldBounds.left != left || oldBounds.top != top
+        || oldBounds.right != right || oldBounds.bottom != bottom) {
+      if (!oldBounds.isEmpty()) {
+        invalidateSelf();
+      }
+
+      mBounds.set(left, top, right, bottom);
+      onBoundsChange(mBounds);
+    }
+  }
+
+  @Override
+  public void setBounds(@NonNull Rect bounds) {
+    setBounds(bounds.left, bounds.top, bounds.right, bounds.bottom);
+  }
+
+  @Override
+  public final void copyBounds(@NonNull Rect dst) {
+    dst.set(mBounds);
+  }
+
+  @Override
+  public final Rect copyBounds() {
+    return new Rect(mBounds);
+  }
+
+  @NonNull
+  @Override
+  public final Rect getBounds() {
+    if (mBounds == ZERO_BOUNDS_RECT) {
+      mBounds = new Rect();
+    }
+
+    return mBounds;
+  }
+
+  /**
+   * Called when the bounds of this drawable has changed. Override this method
+   * if the drawables appearance is based on the bounds.
+   *
+   * <p>Note: For efficiency, the passed {@code bounds} is the same object
+   * stored in the drawable. <strong>You should not modify the object passed
+   * into this method.</strong>
+   *
+   * @see #setBounds(int, int, int, int)
+   * @see #setBounds(Rect)
+   */
+  protected void onBoundsChange(@NonNull Rect bounds) {}
+
+  /**
+   * {@inheritDoc}
+   * <p>
+   * <p>Note: By default, this returns the {@linkplain #getBounds full drawable
+   * bounds}. Custom drawables may override this method to perform more precise
+   * invalidation.
+   */
+  @NonNull
+  @Override
+  public Rect getDirtyBounds() {
+    return getBounds();
+  }
+
+  @Override
+  public boolean getPadding(@NonNull Rect dst) {
+    dst.set(0, 0, 0, 0);
+    return false;
+  }
+
+  @NonNull
+  @Override
+  public Insets getOpticalInsets() {
+    return Insets.NONE;
+  }
+
+  @Override
+  public int getLayoutDirection() {
+    return mLayoutDirection;
+  }
+
+  @Override
+  public void setLayoutDirection(@ResolvedLayoutDir int layoutDirection) {
+    // TODO: This seems like an odd choice, may be due to better compat.
+    if (getLayoutDirection() != layoutDirection) {
+      mLayoutDirection = layoutDirection;
+    }
+  }
+
+  @Override
+  public int getAlpha() {
+    return 0xFF;
+  }
+
+  @Override
+  public boolean isStateful() {
+    return false;
+  }
+
+  @Override
+  public boolean setState(@NonNull int[] stateSet) {
+    if (!Arrays.equals(mStateSet, stateSet)) {
+      mStateSet = stateSet;
+      return onStateChange(stateSet);
+    }
+
+    return false;
+  }
+
+  @NonNull
+  @Override
+  public int[] getState() {
+    return mStateSet;
+  }
+
+  @Override
+  public void jumpToCurrentState() {}
+
+  /**
+   * Called when the state of this drawable has changed. Override this method
+   * if the drawable recognizes the specified state.
+   *
+   * @param state The new state of this drawable
+   *
+   * @return {@code true} if the appearance of the drawable has changed as a
+   *         result of this new state set (that is, it needs to be drawn),
+   *         {@code false} otherwise
+   *
+   * @see #setState(int[])
+   */
+  protected boolean onStateChange(@NonNull int[] state) {
+    return false;
+  }
+
+  @NonNull
+  @Override
+  public Drawable getCurrent() {
+    return this;
+  }
+
+  @Override
+  public final int getLevel() {
+    return mLevel;
+  }
+
+  @Override
+  public final boolean setLevel(@IntRange(from = 0, to = 10000) int level) {
+    // TODO: Validate the range? Android specifies [0..10000], but doesn't enforce
+    if (mLevel != level) {
+      mLevel = level;
+      return onLevelChange(level);
+    }
+
+    return false;
+  }
+
+  /**
+   * Called when the level of this drawable changed. Override this method if
+   * the drawable changes appearance based on level.
+   *
+   * @return {@code true} if the appearance of the drawable has changed as a
+   *         result of this new level (that is, it needs to be drawn),
+   *         {@code false} otherwise
+   *
+   * @see #setLevel(int)
+   */
+  // TODO: level range is not currently enforced, so I don't want to place that restriction here
+  protected boolean onLevelChange(int level) {
+    return false;
+  }
+
+  @Override
+  public boolean setVisible(boolean visible, boolean restart) {
+    boolean changed = mVisible != visible;
+    if (changed) {
+      mVisible = visible;
+      invalidateSelf();
+    }
+
+    return changed;
+  }
+
+  @Override
+  public final boolean isVisible() {
+    return mVisible;
+  }
+
+  @Override
+  public int getIntrinsicWidth() {
+    return -1;
+  }
+
+  @Override
+  public int getIntrinsicHeight() {
+    return -1;
+  }
+
+  @Override
+  public int getMinimumWidth() {
+    final int intrinsicWidth = getIntrinsicWidth();
+    return intrinsicWidth > 0 ? intrinsicWidth : 0;
+  }
+
+  @Override
+  public int getMinimumHeight() {
+    final int intrinsicHeight = getIntrinsicHeight();
+    return intrinsicHeight > 0 ? intrinsicHeight : 0;
+  }
+}
---
+++ src/main/java/unifi/graphics/drawable/Drawable.java
@@ -0,0 +1,445 @@
+package unifi.graphics.drawable;
+
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import unifi.graphics.Canvas;
+import unifi.graphics.Insets;
+import unifi.graphics.PixelFormat;
+import unifi.graphics.Rect;
+import unifi.util.LayoutDirection;
+import unifi.util.LayoutDirection.ResolvedLayoutDir;
+
+import static unifi.graphics.PixelFormat.*;
+
+/**
+ * A {@code Drawable} is a general abstraction for "something that can be
+ * drawn."  Most often you will deal with Drawable as the type of resource
+ * retrieved for drawing things to the screen; Drawable provides an interface
+ * for dealing with an underlying visual resource that may take a variety of
+ * forms. Unlike a {@link unifi.view.View}, a Drawable does not have any
+ * facility to receive events or otherwise interact with the user.
+ *
+ * <p>In addition to simple drawing, Drawable provides a number of generic
+ * mechanisms for its client to interact with what is being drawn:
+ *
+ * <ul>
+ *   <li> The {@link #setBounds} method <var>must</var> be called to tell the
+ *   Drawable where it is drawn and how large it should be. All Drawables should
+ *   respect the requested size, often simply by scaling their imagery. A client
+ *   can find the preferred size for some Drawables with the
+ *   {@link #getIntrinsicHeight} and {@link #getIntrinsicWidth} methods.
+ *
+ *   <li> The {@link #getPadding} method can return from some Drawables
+ *   information about how to frame content that is placed inside of them. For
+ *   example, a Drawable that is intended to be the frame for a button widget
+ *   would need to return padding that correctly places the label inside of
+ *   itself.
+ *
+ *   <li> The {@link #setState} method allows the client to tell the Drawable in
+ *   which state it is to be drawn, such as "focused", "selected", etc. Some
+ *   drawables may modify their imagery based on the selected state.
+ *
+ *   <li> The {@link #setLevel} method allows the client to supply a single
+ *   continuous controller that can modify the Drawable is displayed, such as a
+ *   battery level or progress level. Some drawables may modify their imagery
+ *   based on the current level.
+ *
+ *   <li> A Drawable can perform animations by calling back to its client
+ *   through the {@link Callback} interface.  All clients should support this
+ *   interface (via {@link #setCallback}) so that animations will work. A simple
+ *   way to do this is through the system facilities such as
+ *   {@link unifi.view.View#setBackground(Drawable)} and
+ *   {@link unifi.widget.ImageView}.
+ * </ul>
+ *
+ * TODO: Much of this documentation is irrelevant, however it's here until it potentially is
+ * Though usually not visible to the application, Drawables may take a variety
+ * of forms:
+ *
+ * <ul>
+ *   <li> <b>Bitmap</b>: the simplest Drawable, a PNG or JPEG image.
+ *   <li> <b>Nine Patch</b>: an extension to the PNG format allows it to specify
+ *   information about how to stretch it and place things inside of it.
+ *   <li> <b>Shape</b>: contains simple drawing commands instead of a raw
+ *   bitmap, allowing it to resize better in some cases.
+ *   <li> <b>Layers</b>: a compound drawable, which draws multiple underlying
+ *   drawables on top of each other.
+ *   <li> <b>States</b>: a compound drawable that selects one of a set of
+ *   drawables based on its state.
+ *   <li> <b>Levels</b>: a compound drawable that selects one of a set of
+ *   drawables based on its level.
+ *   <li> <b>Scale</b>: a compound drawable with a single child drawable, whose
+ *   overall size is modified based on the current level.
+ * </ul>
+ */
+public interface Drawable {
+  /**
+   * Draws this drawable onto the passed {@code canvas}.
+   */
+  void draw(@NonNull Canvas canvas);
+
+  /**
+   * Binds the specified {@link Callback} to this drawable. This is required for
+   * clients who want to support animated drawables.
+   */
+  void setCallback(@NonNull Callback callback);
+
+  /**
+   * Returns the current {@link Callback} implementation attached to this
+   * drawable.
+   */
+  @Nullable
+  Callback getCallback();
+
+  /**
+   * Invalidates this drawable using the current {@linkplain #getCallback
+   * callback}. An invalid drawable will need to be redrawn. If there is no
+   * current callback attached to this drawable (i.e., {@code getCallback()}
+   * returns {@code null}), then this method does nothing.
+   *
+   * @see Callback#invalidateDrawable(Drawable)
+   * @see #getCallback()
+   * @see #setCallback(Callback)
+   */
+  void invalidateSelf();
+
+  /**
+   * Schedules this drawable using the current {@linkplain #getCallback
+   * callback}. If there is no current callback attached to this drawable (i.e.,
+   * {@code getCallback()} returns {@code null}), then this method does nothing.
+   *
+   * @param what The action being scheduled
+   * @param when The time (in milliseconds) to run
+   */
+  void scheduleSelf(@NonNull Runnable what, long when);
+
+  /**
+   * Unschedules this drawable using the current {@linkplain #getCallback
+   * callback}. If there is no current callback attached to this drawable (i.e.,
+   * {@code getCallback()} returns {@code null}), then this method does nothing.
+   *
+   * @param what The runnable that you no longer want called
+   */
+  void unscheduleSelf(@NonNull Runnable what);
+
+  /**
+   * Specifies the bounding rectangle for this drawable. This is where the
+   * drawable will draw when {@link #draw} is called. All values are relative to
+   * the top left of the canvas this drawable will draw itself on.
+   */
+  void setBounds(int left, int top, int right, int bottom);
+
+  /**
+   * Specifies the bounding rectangle for this drawable. This is where the
+   * drawable will draw when {@link #draw} is called.
+   *
+   * <p>Note: The bounds are only copied from {@code bounds}, so no reference is
+   * kept.
+   */
+  void setBounds(@NonNull Rect bounds);
+
+  /**
+   * Copies the bounds of this drawable into the specified {@code dst} rectangle
+   * (allocated by the caller). The bounds specify where this drawable will draw
+   * when its {@link #draw} method is called.
+   *
+   * <p>Note: {@code dst} is only a copy and will not update as this drawable's
+   * bounds change.
+   *
+   * @see #copyBounds()
+   */
+  void copyBounds(@NonNull Rect dst);
+
+  /**
+   * Returns a copy of the bounds of this drawable in a newly allocated
+   * rectangle. This returns the same values as {@link #getBounds}, but the
+   * returned object is guaranteed to not be changed later by the drawable
+   * (i.e., it retains no reference to this drawable's bounding rectangle).
+   * If the caller already has a {@link Rect} allocated, use
+   * {@link #copyBounds(Rect)} instead.
+   *
+   * @see #copyBounds(Rect)
+   */
+  Rect copyBounds();
+
+  /**
+   * Returns the bounds of this drawable as a {@link Rect}.
+   *
+   * <p>Note: For efficiency, the returned object may be the same object stored
+   * in the drawable (though this is not guaranteed), so if a persistent copy of
+   * the bounds is needed, call {@link #copyBounds(Rect)} instead.
+   *
+   * <p>Note: <strong>You should not modify the object returned by this method,
+   * as it may be the same object stored in the drawable.</strong>
+   */
+  @NonNull
+  Rect getBounds();
+
+  /**
+   * Returns this drawable's dirty bounds as a {@link Rect}.
+   *
+   * <p>Note: For efficiency, the returned object may be the same object stored
+   * in the drawable (though this is not guaranteed).
+   */
+  @NonNull
+  Rect getDirtyBounds();
+
+  /**
+   * Populates the specified rectangle with the insets suggested by this
+   * drawable for placing content inside the its bounds. Positive values move
+   * toward the center of the drawable.
+   *
+   * <p>Note: If {@code true} is returned, then this drawable has no padding,
+   * i.e., the padding on each side is equal to {@code 0}.
+   *
+   * @return {@code true} if this drawable actually has padding, {@code false}
+   *         otherwise
+   *
+   * @see Rect#inset(int, int)
+   */
+  boolean getPadding(@NonNull Rect dst);
+
+  /**
+   * Returns the insets suggested by this drawable for use with alignment
+   * operations during layout.
+   */
+  @NonNull
+  Insets getOpticalInsets();
+
+  /**
+   * Gets the opacity/transparency of this drawable. The returned value is one
+   * of the abstract format constants in {@link unifi.graphics.PixelFormat}:
+   * {@link PixelFormat#UNKNOWN}, {@link PixelFormat#TRANSLUCENT},
+   * {@link PixelFormat#TRANSPARENT}, or {@link PixelFormat#OPAQUE}.
+   *
+   * <p>Generally a drawable should be as conservative as possible with the
+   * value it returns. For example, if it contains multiple child drawables and
+   * only shows one of them at a time, if only one of the children is {@code
+   * TRANSLUCENT} and the others are {@code OPAQUE} then {@code TRANSLUCENT}
+   * should be returned. You can use {@link PixelFormat#resolveOpacity} to
+   * perform a standard reduction of two opacities to the appropriate single
+   * output.
+   *
+   * TODO: Document setColorFilter?:
+   * <p>Note that the returned value does <em>not</em> take into account a
+   * custom alpha or color filter that has been applied by the client through
+   * the {@link #setAlpha} or {@link #setColorFilter} methods.
+   *
+   * @return The opacity pixel format of the drawable.
+   *
+   * @see unifi.graphics.PixelFormat
+   */
+  @Opacity
+  int getOpacity();
+
+  /**
+   * Returns the resolved layout direction for this drawable.
+   *
+   * @return One of {@link LayoutDirection#LTR}, {@link LayoutDirection#RTL}
+   */
+  @ResolvedLayoutDir
+  int getLayoutDirection();
+
+  /**
+   * Sets the layout direction for this drawable. {@code layoutDirection} should
+   * already be resolved -- it is not the drawables responsibility to perform
+   * this resolution.
+   *
+   * @param layoutDirection One of {@link LayoutDirection#LTR},
+   *                               {@link LayoutDirection#RTL}
+   */
+  void setLayoutDirection(@ResolvedLayoutDir int layoutDirection);
+
+  /**
+   * Gets the alpha value for this drawable in the range {@code [0..255]}, where
+   * {@code 0} indicates fully transparent and {@code 255} indicates fully
+   * opaque.
+   */
+  @IntRange(from = 0, to = 255)
+  int getAlpha();
+
+  /**
+   * Sets the alpha value for the drawable in the range {@code [0..255]}, where
+   * {@code 0} indicates fully transparent and {@code 255} indicates fully
+   * opaque.
+   */
+  void setAlpha(@IntRange(from = 0, to = 255) int alpha);
+
+  /**
+   * Indicates whether or not this drawable will change its appearance based on
+   * its state. Clients can use this to determine whether it is necessary to
+   * calculate their state and call {@link #setState(int[])}.
+   */
+  boolean isStateful();
+
+  /**
+   * Specifies a new set of states for the drawable. These are use-case
+   * specific, so see the relevant documentation.
+   * TODO: Document Button states as example
+   *
+   * <p>If the new {@code stateSet} being applied causes the appearance of the
+   * drawable to change, then it is responsible for calling
+   * {@link #invalidateSelf()} in order do have itself redrawn and this method
+   * should return {@code true}.
+   *
+   * <p>Note: The drawable keeps a reference to {@code stateSet} until a new
+   * state set is specified via another call to this method, so you <strong>must
+   * not modify this array during that time</strong>.
+   *
+   * @param stateSet The new set of states to apply
+   *
+   * @return {@code true} if the appearance of the drawable changes as a result
+   *         of this operation, {@code false} otherwise
+   */
+  boolean setState(@NonNull int[] stateSet);
+
+  /**
+   * Describes the current state, as a union of primitive states. Some drawables
+   * may modify their imagery based on the selected state.
+   *
+   * @return An array of resource IDs describing the current state
+   */
+  @NonNull
+  int[] getState();
+
+  /**
+   * Asks the drawable to immediately jump to the current state and skip any
+   * active animations if it does transition animations between states.
+   */
+  void jumpToCurrentState();
+
+  /**
+   * TODO: Document StateListDrawable and LevelListDrawable
+   * The current drawable that will be used by this drawable. For simple
+   * drawables, this is just the drawable itself, however for drawable that
+   * change state, this will be the child drawable currently in use.
+   */
+  @NonNull
+  Drawable getCurrent();
+
+  /**
+   * Specifies the level for the drawable. This allows a drawable to vary its
+   * imagery based on a continuous controller, for example to show progress or
+   * volume level.
+   *
+   * <p>If the new level you are supplying causes the appearance of the drawable
+   * to change, then it is responsible for calling {@link #invalidateSelf()} in
+   * order to have itself redrawn, and this method should return {@code true}.
+   *
+   * @param level The new level, in the range {@code [0..10000]}
+   *
+   * @return {@code true} if the appearance of this drawable changed as a result
+   *         of this method, {@code false} otherwise
+   */
+  boolean setLevel(@IntRange(from = 0, to = 10000) int level);
+
+  /**
+   * Returns the current level of the drawable in the range {@code [0..10000]}.
+   *
+   * @see #setLevel(int)
+   */
+  @IntRange(from = 0, to = 10000)
+  int getLevel();
+
+  /**
+   * Sets whether or not this drawable is visible. This generally does not
+   * impact the drawable's behavior, but is a hint that can be used by some
+   * drawables, for example, to decide whether or not to run animations.
+   *
+   * @param visible {@code true} to set this drawable as visible
+   * @param restart {@code true} to force the drawable to behave as if it has
+   *                just become visible, even if it had last been visible. This
+   *                can be used to force animations to restart.
+   *
+   * @return {@code true} if the new visibility is different from its previous
+   *         state, {@code false} otherwise
+   */
+  boolean setVisible(boolean visible, boolean restart);
+
+  /**
+   * Indicates whether or not this drawable is visible.
+   */
+  boolean isVisible();
+
+  /**
+   * Returns the intrinsic width of the underlying drawable object, or {@code
+   * -1} if it has no intrinsic width, such as with a solid color.
+   */
+  int getIntrinsicWidth();
+
+  /**
+   * Returns the intrinsic height of the underlying drawable object, or {@code
+   * -1} if it has no intrinsic height, such as with a solid color.
+   */
+  int getIntrinsicHeight();
+
+  /**
+   * Returns the minimum width suggested by this drawable. If this drawable is
+   * used as a background, it is suggested that the parent use at least this
+   * value for its width.
+   *
+   * <p>Note: There will be some scenarios where this will not be possible.
+   * <p>Note: This value should <em>include</em> any padding.
+   *
+   * @return The minimum width suggested by this drawable, or {@code 0} if this
+   *         drawable doesn't have a suggested minimum width
+   */
+  int getMinimumWidth();
+
+  /**
+   * Returns the minimum height suggested by this drawable. If this drawable is
+   * used as a background, it is suggested that the parent use at least this
+   * value for its height.
+   * <p>
+   * <p>Note: There will be some scenarios where this will not be possible.
+   * <p>Note: This value should <em>include</em> any padding.
+   *
+   * @return The minimum height suggested by this drawable, or {@code 0} if this
+   *         drawable doesn't have a suggested minimum height
+   */
+  int getMinimumHeight();
+
+  /**
+   * Defines the behavior of an animated drawable. Upon retrieving a drawable,
+   * use {@link Drawable#setCallback} to supply your implementation of this
+   * interface to the drawable; it uses this interface to schedule and execute
+   * animation changes.
+   */
+  interface Callback {
+    /**
+     * Called when the drawable needs to be redrawn. A parent should invalidate
+     * itself at this point (or at least the part of itself where the drawable
+     * appears).
+     *
+     * @param who The drawable initiating the request
+     */
+    void invalidateDrawable(@NonNull Drawable who);
+
+    /**
+     * Schedules the next frame of the drawable's animation. An implementation
+     * can generally call {@link unifi.runtime.Timer#postAtTime(Runnable,
+     * Object, long)} with the parameters {@code what, who, when} to perform the
+     * scheduling.
+     *
+     * @param who  The drawable being scheduled
+     * @param what The action to execute
+     * @param when The time (in milliseconds) to run. Timebase is {@link
+     *             unifi.runtime.SystemClock#millisTime()}
+     */
+    void scheduleDrawable(@NonNull Drawable who, @NonNull Runnable what, long
+        when);
+
+    /**
+     * Unschedules an action previously scheduled with
+     * {@link #scheduleDrawable}. An implementation can generally call
+     * {@link unifi.runtime.Timer#removeCallbacks(Runnable, Object)} with the
+     * parameters {@code what, who} to unschedule the drawable.
+     *
+     * @param who  The drawable being unscheduled
+     * @param what The action being unscheduled
+     */
+    void unscheduleDrawable(@NonNull Drawable who, @NonNull Runnable what);
+  }
+}
