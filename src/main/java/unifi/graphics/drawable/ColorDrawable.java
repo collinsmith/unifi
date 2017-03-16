494b39134cc30e2c5582a0a5c03d05dc525a9476
Collin Smith <collinsmith70@gmail.com>
2017-03-16 15:45:50 -0700

    Added ConstantState impl

--- src/main/java/unifi/graphics/drawable/ColorDrawable.java
+++ src/main/java/unifi/graphics/drawable/ColorDrawable.java
@@ -16,13 +16,15 @@ public class ColorDrawable extends AbstractDrawable {
 
   private static final Paint sPaint = new Paint();
 
-  @ColorInt private int mColor;
+  @NonNull
+  private ConstantState mColorState;
+  private boolean mMutated;
 
   /**
    * Constructs a color drawable that will draw in black.
    */
-  public ColorDrawable() {
-    mColor = Color.BLACK;
+  ColorDrawable() {
+    mColorState = new ConstantState();
   }
 
   /**
@@ -31,7 +33,8 @@ public class ColorDrawable extends AbstractDrawable {
    * @param color The color to draw
    */
   public ColorDrawable(@ColorInt int color) {
-    mColor = color;
+    this();
+    setColor(color);
   }
 
   /**
@@ -39,30 +42,47 @@ public class ColorDrawable extends AbstractDrawable {
    */
   @ColorInt
   public int getColor() {
-    return mColor;
+    return mColorState.mUseColor;
   }
 
   /**
    * Sets the color of this drawable to the specified {@link Color}.
    */
   public void setColor(@ColorInt int color) {
-    if (mColor != color) {
-      mColor = color;
+    if (mColorState.mBaseColor != color || mColorState.mUseColor != color) {
+      mColorState.mBaseColor = mColorState.mUseColor = color;
       invalidateSelf();
     }
   }
 
+  @NonNull
+  @Override
+  public Drawable mutate() {
+    if (!mMutated && super.mutate() == this) {
+      mColorState = new ConstantState(mColorState);
+      mMutated = true;
+    }
+
+    return this;
+  }
+
+  @Override
+  public void clearMutated() {
+    super.clearMutated();
+    mMutated = false;
+  }
+
   @Override
   public void draw(@NonNull Canvas canvas) {
-    if (Color.alpha(sPaint.getColor()) != 0) {
-      sPaint.setColor(mColor);
+    if (Color.alpha(mColorState.mUseColor) != 0) {
+      sPaint.setColor(mColorState.mUseColor);
       canvas.drawRect(getBounds(), sPaint);
     }
   }
 
   @Override
   public int getOpacity() {
-    switch (Color.alpha(mColor)) {
+    switch (Color.alpha(mColorState.mUseColor)) {
       case 0:   return PixelFormat.TRANSPARENT;
       case 255: return PixelFormat.OPAQUE;
       default:  return PixelFormat.TRANSLUCENT;
@@ -71,11 +91,47 @@ public class ColorDrawable extends AbstractDrawable {
 
   @Override
   public int getAlpha() {
-    return Color.alpha(mColor);
+    return Color.alpha(mColorState.mUseColor);
   }
 
   @Override
   public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
-    mColor = Color.setAlpha(mColor, alpha);
+    alpha += alpha >> 7; // make it 0..256
+    final int baseAlpha = Color.alpha(mColorState.mBaseColor);
+    final int useAlpha = baseAlpha * alpha >> 8;
+    final int useColor = Color.setAlpha(mColorState.mBaseColor, useAlpha);
+    if (mColorState.mUseColor != useColor) {
+      mColorState.mUseColor = useColor;
+      invalidateSelf();
+    }
+  }
+
+  @Override
+  protected boolean onStateChange(@NonNull int[] stateSet) {
+    return false;
+  }
+
+  @Override
+  public boolean isStateful() {
+    return false;
+  }
+
+  private ColorDrawable(@NonNull ConstantState state) {
+    assert state != null;
+    mColorState = state;
+  }
+
+  final static class ConstantState implements Drawable.ConstantState {
+    /** Base color, independent of  {@link #setAlpha(int)} */
+    @ColorInt int mBaseColor;
+    /** Base color, modulated by {@link #setAlpha(int)} */
+    @ColorInt int mUseColor;
+
+    ConstantState() {}
+
+    ConstantState(@NonNull ConstantState state) {
+      mBaseColor = state.mBaseColor;
+      mUseColor = state.mUseColor;
+    }
   }
 }
