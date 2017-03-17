abf76718ec2624f028191c8ef30011df5cec543e
Collin Smith <collinsmith70@gmail.com>
2017-03-17 16:29:32 -0700

    Refactored constant names to removed excess text

--- src/main/java/unifi/util/FocusDirection.java
+++ src/main/java/unifi/util/FocusDirection.java
@@ -11,55 +11,43 @@ import java.lang.annotation.RetentionPolicy;
  * backward).
  */
 public class FocusDirection {
-  @IntDef({
-      FOCUS_BACKWARD,
-      FOCUS_FORWARD,
-      FOCUS_LEFT,
-      FOCUS_UP,
-      FOCUS_RIGHT,
-      FOCUS_DOWN
-  })
+  @IntDef({ BACKWARD, FORWARD, LEFT, UP, RIGHT, DOWN })
   @Retention(RetentionPolicy.SOURCE)
   public @interface Any {}
 
-  @IntDef({
-      FOCUS_LEFT,
-      FOCUS_UP,
-      FOCUS_RIGHT,
-      FOCUS_DOWN
-  })
+  @IntDef({ LEFT, UP, RIGHT, DOWN })
   @Retention(RetentionPolicy.SOURCE)
   public @interface Real {} // Like @Any, but without forward/backward
 
   /**
    * Move focus to the previous selectable item.
    */
-  public static final int FOCUS_BACKWARD = 0x00000001;
+  public static final int BACKWARD = 0x00000001;
 
   /**
    * Move focus to the next selectable item.
    */
-  public static final int FOCUS_FORWARD = 0x00000002;
+  public static final int FORWARD = 0x00000002;
 
   /**
    * Move focus to the left.
    */
-  public static final int FOCUS_LEFT = 0x00000011;
+  public static final int LEFT = 0x00000011;
 
   /**
    * Move focus up.
    */
-  public static final int FOCUS_UP = 0x00000021;
+  public static final int UP = 0x00000021;
 
   /**
    * Move focus to the right.
    */
-  public static final int FOCUS_RIGHT = 0x00000042;
+  public static final int RIGHT = 0x00000042;
 
   /**
    * Move focus down.
    */
-  public static final int FOCUS_DOWN = 0x00000082;
+  public static final int DOWN = 0x00000082;
 
   private FocusDirection() {}
 
