00c8462bde7aeed43296fce942695205ae00de60
Collin Smith <collinsmith70@gmail.com>
2017-03-28 23:51:13 -0700

    Added assetmanager field

--- src/main/java/unifi/content/res/Resources.java
+++ src/main/java/unifi/content/res/Resources.java
@@ -1,8 +1,30 @@
 package unifi.content.res;
 
+import android.support.annotation.NonNull;
+
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.utils.Disposable;
 
 public class Resources implements Disposable {
+
+  @NonNull static final String TAG = "Resources";
+
+  @NonNull final AssetManager mAssets;
+
+  private Resources() {
+    mAssets = new AssetManager();
+  }
+
   @Override
-  public void dispose() {}
+  public void dispose() {
+    mAssets.dispose();
+  }
+
+  /**
+   * Returns the asset manager of these resources.
+   */
+  @NonNull
+  public final AssetManager getAssets() {
+    return mAssets;
+  }
 }
